e5778922103226c45b59516e970910c7d861ccba
Oscar Neira Garcia <oneirag@yahoo.es>
2022-01-22 08:26:22 +0100

    Promote to version 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ong_utils',
-    version='0.2.2',
+    version='0.2.3',
     packages=['ong_utils'],
     url='www.neirapinuela.es',
     license='',
